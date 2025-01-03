019507599f96cbc2c50494a3464d63cc86cdaba8
Siva-S-2007 <168512102+Siva-S-2007@users.noreply.github.com>
2025-01-03 16:46:02 +0530

    Add files via upload

---
+++ techk7.js
@@ -0,0 +1,29 @@
+// Get the button element by its ID
+document.getElementById("rocket").onclick = function() {
+    // Redirect to the YouTube video when the button is clicked
+    window.open("https://youtu.be/m4SntLpLhUw?feature=shared", "_blank"); // Replace with your desired YouTube video link
+};
+
+document.getElementById("indicator").onclick = function() {
+    window.open("https://youtu.be/a1Bktjf9stU?si=mML9bTzKmjTUqP2H","_blank");
+}
+
+document.getElementById("robot").onclick = function() {
+    window.open("https://youtu.be/PGGBppcHctU?si=0adOzMTMuIojJYNV","_blank");
+}
+
+document.getElementById("door").onclick = function() {
+    window.open("https://youtu.be/zO-65IzxlG4?si=sz8HJZTb5G9gGGdv","_blank");
+}
+
+document.getElementById("camera").onclick = function() {
+    window.open("https://youtu.be/ijRTZHamIOM?si=8EM2r93BPNDryFH_","_blank");
+}
+
+document.getElementById("minipc").onclick = function() {
+    window.open("https://youtu.be/LqO3XBjhwlw?si=l8K5XB4dyCJYo9a8","_blank");
+}
+
+document.getElementById("contact-btn").onclick = function() {
+    location.href = '#contact';
+}
